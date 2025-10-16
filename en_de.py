87e1ce8ea908af67eb340322eebb7732f9988dc8
Leesha <leeshachavda3@gamil.com>
2025-10-16 19:04:43 +0530

    Initial commit

---
+++ en_de.py
@@ -0,0 +1,25 @@
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+def encrypt(text1,shift1):  
+    encrypt_text = ""
+    for i in text1:
+        encrypt_text += chr(ord(i)+shift1)
+    print(F"This is the encrypted text: {encrypt_text}")    
+def decrypt(text1,shift1):  
+    decrypt_text = ""
+    for i in text1:
+        decrypt_text += chr(ord(i)-shift1)
+    print(F"This is the decrypted text: {decrypt_text}")   
+
+if direction == "encode":
+    encrypt(text,shift)
+elif direction == "decode":
+    decrypt(text,shift)
+else :
+    print("the direction is invalis!!")  
+
+   
+ 
